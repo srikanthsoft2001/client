0fea3d2dc5a25326bba443267e05358a5234d1c9
srikanth <rmsrikanth25@gmail.com>
2025-06-25 20:36:14 +0530

    ----

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { AuthProvider } from '@/context/AuthContext';
 
-import { useAppDispatch, useAppSelector, validateToken } from './Store/store';
+import { useAppDispatch, useAppSelector, validateToken } from './store/store';
 import ProtectedRoute from './routes/ProtectedRoute';
 
 import Layout from '@/components/Layout';
@@ -47,7 +47,6 @@ const CategoryProductsWrapper = () => {
   return <CategoryProducts category={category} subcategory={subcategory} />;
 };
 
-  
 const App = () => {
   const dispatch = useAppDispatch();
   const loading = useAppSelector((state) => state.auth.loading);
@@ -61,68 +60,67 @@ const App = () => {
 
   return (
     <ReduxProvider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Sonner />
-        <AuthProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<Layout />}>
-                {/* Public Routes */}
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/search" element={<ProductSearch />} />
-                <Route path="/products/:id" element={<ProductPage />} />
-                <Route path="/all-products" element={<AllProductsPage />} />
-                <Route path="/dashboard" element={<DashboardLayout />}>
-                  <Route index element={<DashboardPage />} />
-                </Route>
-                <Route path="/food" element={<FoodPage />} />
-                <Route path="/electronics" element={<ElectronicsPage />} />
-                <Route path="/realestate" element={<RealEstatePage />} />
-                <Route
-                  path="/Medical & Para Medical"
-                  element={<MedicalPage />}
-                />
-                <Route
-                  path="/category/:category"
-                  element={<CategoryProductsWrapper />}
-                />
-                <Route
-                  path="/category/:category/:subcategory"
-                  element={<CategoryProductsWrapper />}
-                />
-                <Route path="/account" element={<AccountLayout />} />
-                <Route path="/order-success" element={<OrderSuccess />} />
-                <Route
-                  path="/dashboard/order-list"
-                  element={<OrderListPage />}
-                />
-
-                {/* Protected Routes */}
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/wishlist" element={<WishlistPage />} />
-                  <Route path="/cart" element={<Cart />} />
-                  <Route path="/checkout" element={<CheckoutLayout />} />
-                  <Route path="/paymentmethod" element={<PaymentMethod />} />
-                  <Route path="/account" element={<AccountLayout />} />
-                  <Route path="/order-list" element={<OrderListPage />} />
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Sonner />
+          <AuthProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route element={<Layout />}>
+                  {/* Public Routes */}
+                  <Route path="/" element={<Home />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/search" element={<ProductSearch />} />
+                  <Route path="/products/:id" element={<ProductPage />} />
+                  <Route path="/all-products" element={<AllProductsPage />} />
                   <Route path="/dashboard" element={<DashboardLayout />}>
                     <Route index element={<DashboardPage />} />
                   </Route>
+                  <Route path="/food" element={<FoodPage />} />
+                  <Route path="/electronics" element={<ElectronicsPage />} />
+                  <Route path="/realestate" element={<RealEstatePage />} />
+                  <Route
+                    path="/Medical & Para Medical"
+                    element={<MedicalPage />}
+                  />
+                  <Route
+                    path="/category/:category"
+                    element={<CategoryProductsWrapper />}
+                  />
+                  <Route
+                    path="/category/:category/:subcategory"
+                    element={<CategoryProductsWrapper />}
+                  />
+                  <Route path="/account" element={<AccountLayout />} />
+                  <Route path="/order-success" element={<OrderSuccess />} />
+                  <Route
+                    path="/dashboard/order-list"
+                    element={<OrderListPage />}
+                  />
+
+                  {/* Protected Routes */}
+                  <Route element={<ProtectedRoute />}>
+                    <Route path="/wishlist" element={<WishlistPage />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={<CheckoutLayout />} />
+                    <Route path="/paymentmethod" element={<PaymentMethod />} />
+                    <Route path="/account" element={<AccountLayout />} />
+                    <Route path="/order-list" element={<OrderListPage />} />
+                    <Route path="/dashboard" element={<DashboardLayout />}>
+                      <Route index element={<DashboardPage />} />
+                    </Route>
+                  </Route>
                 </Route>
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </AuthProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
+              </Routes>
+            </BrowserRouter>
+          </AuthProvider>
+        </TooltipProvider>
+      </QueryClientProvider>
     </ReduxProvider>
   );
-  
-);
+};
 
 export default App;
